b647e4d079cfd782cdce0a1c2716de86fae7660b
Ju-LT <julie.nussbaumer@etu.umontpellier.fr>
2022-04-27 10:50:08 +0200

    mvc header et footer

--- view/view.php
+++ view/view.php
@@ -4,10 +4,21 @@
     <meta charset="UTF-8">
     <title><?php echo $pagetitle; ?></title>
 </head>
+<header>
+    <a href='indexx.php?'>Nos good goods</a>
+    <a href='indexx.php?action=readAll&controller=utilisateur'>Accueil utilisateur</a>
+    <a href='indexx.php?action=voirPanier&controller=ControllerPanier'>Mon panier</a>
+
+</header>
 <body>
 <?php
 $filepath = File::build_path(array("view", "$view.php"));
 require $filepath;
 ?>
 </body>
+<footer>
+    <p style="border: 1px solid black;text-align:right;padding-right:1em;">
+        Site de Yvang et Djulie
+    </p>
+</footer>
 </html>
